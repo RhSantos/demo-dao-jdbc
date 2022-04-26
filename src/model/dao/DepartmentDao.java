0dbb5b02aeb4ce83f73460a5b7ceae349414de54
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-26 17:33:17 -0300

    Data Acess Object das Entidades

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,12 @@
+package src.model.dao;
+
+import java.util.List;
+import src.model.entities.Department;
+
+public interface DepartmentDao {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
