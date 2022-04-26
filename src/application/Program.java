2076bdb87c426633f75d3cc2382744d75c543b94
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-26 17:24:32 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package src.application;
 
+import java.util.Date;
+
 import src.model.entities.Department;
+import src.model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Book");
+        Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),2900.0,obj);
         System.out.println(obj);
+        System.out.println(seller);
     } 
 }
