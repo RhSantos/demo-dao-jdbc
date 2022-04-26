ea4fadf4888e6e4e2d6e5383ba2844d2e2b4d0ab
RhSantos <rhuan.santos.araujo@gmail.com>
2022-04-26 18:08:37 -0300

    Implementação FindById() SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,15 @@ package src.application;
 
 import java.util.Date;
 
-import src.model.entities.Department;
+import src.model.dao.DaoFactory;
+import src.model.dao.SellerDao;
 import src.model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1,"Book");
-        Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),2900.0,obj);
-        System.out.println(obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
     } 
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package src.model.dao;
 
+import src.db.DB;
 import src.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 
     // public static DepartmentDao createDepartmentDao(){
